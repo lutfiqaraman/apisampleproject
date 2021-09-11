5a144509b28099d8400d80cef6516453ee2b440a
lutfiqaraman <lutfiqaraman@gmail.com>
2021-09-11 13:17:32 +0300

    Add project files.

---
+++ AppConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apisampleproject
+{
+    public class AppConfiguration
+    {
+        public string ApiKey { get; set; }
+    }
+}
---
+++ Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apisampleproject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        AppConfiguration AppConfiguration;
+
+        public TestController(AppConfiguration appConfiguration)
+        {
+            AppConfiguration = appConfiguration;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return AppConfiguration.ApiKey;
+        }
+    }
+}
